5314c08982a6c57eef42a0c18ba977b774550597
Mario Gomez <mariogmez12345@gmail.com>
2025-10-07 01:17:56 -0600

    modify main.py and testing lazygit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def main():
+    book_path = "books/frankenstein.txt"
+    book_content = get_book_text(book_path)
+    print(book_content)
+
+main()
+    
+
